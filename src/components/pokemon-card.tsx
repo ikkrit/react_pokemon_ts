4a3daaa30ea6ee0c20fc4ef03f5c28a11075e3b5
ikkrit <johan.car@protonmail.com>
2023-04-06 22:56:51 +0200

    helpers

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -1,22 +1,83 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import Pokemon from '../models/pokemon';
+import './pokemon-card.css';
+import formatDate from '../helpers/format-date';
   
 type Props = {
-  pokemon: Pokemon
+  pokemon: Pokemon,
+  borderColor?: string
 };
   
-const PokemonCard: FunctionComponent<Props> = ({pokemon}) => {
-    
+const PokemonCard: FunctionComponent<Props> = ({pokemon, borderColor = '#009688'}) => {
+
+  const [color, setColor] = useState<string>();
+
+  const showBorder = () => {
+    setColor(borderColor);
+  }
+
+  const hideBorder = () => {
+    setColor('#f5f5f5');
+  }
+
+  const formatType = (type: string): string => {
+    let color: string;
+   
+    switch (type) {
+      case 'Feu': 
+        color = 'red lighten-1'; 
+        break; 
+      case 'Eau': 
+        color = 'blue lighten-1'; 
+        break; 
+      case 'Plante': 
+        color = 'green lighten-1'; 
+        break; 
+      case 'Insecte': 
+        color = 'brown lighten-1'; 
+        break; 
+      case 'Normal': 
+        color = 'grey lighten-3'; 
+        break; 
+      case 'Vol': 
+        color = 'blue lighten-3'; 
+        break; 
+      case 'Poison': 
+        color = 'deep-purple accent-1'; 
+        break; 
+      case 'Fée': 
+        color = 'pink lighten-4'; 
+        break; 
+      case 'Psy': 
+        color = 'deep-purple darken-2'; 
+        break; 
+      case 'Electrik': 
+        color = 'lime accent-1'; 
+        break; 
+      case 'Combat': 
+        color = 'deep-orange'; 
+        break; 
+      default: 
+        color = 'grey'; 
+        break; 
+    }
+   
+    return `chip ${color}`;
+  }
+
   return (
-    <div className="col s6 m4">
-      <div className="card horizontal">
+    <div className="col s6 m4" onMouseEnter={showBorder} onMouseLeave={hideBorder}>
+      <div className="card horizontal" style={{ borderColor: color }}>
         <div className="card-image"> 
           <img src={pokemon.picture} alt={pokemon.name}/>
         </div>
         <div className="card-stacked">
           <div className="card-content">
             <p>{pokemon.name}</p>
-            <p><small>{pokemon.created.toString()}</small></p>
+            <p><small>{formatDate(pokemon.created)}</small></p>
+            {pokemon.types.map(type => (
+                <span key={type} className={formatType(type)}>{type}</span>
+            ))}
           </div>
         </div>
       </div> 
